a2bea82ec3f8e431e8a7188cb98ff9dd516818e6
92euni <92_sudal@naver.com>
2022-10-07 00:57:22 +0900

    오류수정 - xml에서 values에 ()가 빠져있었음

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,27 +1,20 @@
 const registerButton = document.querySelector(".login-button");
 const registerInputs = document.querySelectorAll(".login-input");
 
-
 //enter누르면 회원가입 되기!
-for(let i = 0; i< registerInputs.length; i++){
+for(let i = 0; i < registerInputs.length; i++) {
     registerInputs[i].onkeyup = () => {
-            if(window.event.keyCode == 13){
-                if(i != 3){
-                    registerInputs[i + 1].focus();
-                }else{
-                    registerButton.click();
-                }
+        if(window.event.keyCode == 13){
+            if(i != 3) {
+                registerInputs[i + 1].focus();
+            }else {
+                registerButton.click();
             }
+        }
     }
 }
-//submit버튼이면 이렇게 안써도 enter가 먹힌다
-
-
-
 
 registerButton.onclick = () => {
-
-    
 
     let registerInfo = {
         lastName: registerInputs[0].value,
@@ -31,38 +24,34 @@ registerButton.onclick = () => {
     }
 
     $.ajax({
-        async:false,
-        type:"post",
-        url:"/api/account/register",
-        contentType:"application/json",
+        async: false,
+        type: "post",
+        url: "/api/account/register",
+        contentType: "application/json",
         data: JSON.stringify(registerInfo),
-//     JSON.stringify :  obj-> json 으로 바꿔줌
         dataType: "json",
-        success:(response) => {
+        success: (response) => {
             location.replace("/account/login");
-
         },
-        error:(error) => {
+        error: (error) => {
             console.log(error);
             validationError(error.responseJSON.data);
         }
-
-    })
+    });
 }
 
 function validationError(error) {
     const accountErrors = document.querySelector(".account-errors");
     const accountErrorList = accountErrors.querySelector("ul");
 
-    const errorsValues = Object.values(error);
+    const errorValues = Object.values(error);
 
     accountErrorList.innerHTML = "";
 
-    errorsValues.forEach((value) => {
+    errorValues.forEach((value) => {
         accountErrorList.innerHTML += `
-                <li>${value}</li>
+            <li>${value}</li>
         `;
-
     });
 
     accountErrors.classList.remove("errors-invisible");
