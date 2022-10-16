fba65180c9a6fc59b7b329829b1fdf9f2b8bcf48
92euni <92_sudal@naver.com>
2022-10-17 01:02:40 +0900

    product-list 정보 가져오기 ing~

--- src/main/java/com/stussy/stussyclone20220929jungeun/Controller/admin/api/ProductApi.java
+++ src/main/java/com/stussy/stussyclone20220929jungeun/Controller/admin/api/ProductApi.java
@@ -8,13 +8,13 @@ import com.stussy.stussyclone20220929jungeun.dto.admin.ProductAdditionReqDto;
 import com.stussy.stussyclone20220929jungeun.dto.validation.ValidationSequence;
 import com.stussy.stussyclone20220929jungeun.exception.CustomInternalServerErrorException;
 import com.stussy.stussyclone20220929jungeun.service.admin.ProductService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/admin")
 @RestController
@@ -26,11 +26,30 @@ public class ProductApi {
     @ValidAspect
     @LogAspect
     @PostMapping("/product")
-    public ResponseEntity<?> addProduct(@Validated(ValidationSequence.class) ProductAdditionReqDto productAdditionReqDto, BindingResult bindingResult) throws Exception{
-        //Exception 달아서 try catch안해도 됨 
+    public ResponseEntity<?> addProduct(@Validated(ValidationSequence.class) ProductAdditionReqDto productAdditionReqDto, BindingResult bindingResult) throws Exception {
+        //Exception 달아서 try catch안해도 됨
+
+//        String productName = productAdditionReqDto.getName();
+//
+//        for(int i =0; i < 200; i++){
+//            if(i % 4 == 0){
+//                productAdditionReqDto.setName( productName + "-" + (i + 1) );
+//            }
+//            productService.addProduct(productAdditionReqDto);
+//        }
+
+
         return ResponseEntity
                 .created(null)
-                .body(new CMRespDto<>(1, "successfully",  productService.addProduct(productAdditionReqDto)));
+                .body(new CMRespDto<>(1, "successfully", productService.addProduct(productAdditionReqDto)));
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<?> getProductList(@RequestParam int pageNumber,
+                                            @RequestParam @Nullable String category,
+                                            @RequestParam @Nullable String searchText) throws Exception {
+
+        return ResponseEntity.ok(new CMRespDto<>(1, "Successfully", productService.getProductList(pageNumber, category, searchText)));
     }
 }
 
--- src/main/java/com/stussy/stussyclone20220929jungeun/service/admin/ProductService.java
+++ src/main/java/com/stussy/stussyclone20220929jungeun/service/admin/ProductService.java
@@ -1,8 +1,14 @@
 package com.stussy.stussyclone20220929jungeun.service.admin;
 
+import com.stussy.stussyclone20220929jungeun.domain.Product;
 import com.stussy.stussyclone20220929jungeun.dto.admin.ProductAdditionReqDto;
+
+import java.util.List;
 
 public interface ProductService {
 
+
     public boolean addProduct(ProductAdditionReqDto productAdditionReqDto) throws Exception;
+
+    public List<Product> getProductList(int pageNumber, String category, String searchText) throws Exception;
 }
